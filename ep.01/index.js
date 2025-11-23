02af1be843b06804cd16931915f1bb2b8e3b1538
soran <soran.taak@gmail.com>
2025-11-23 13:05:19 +0330

    slide show

---
+++ ep.01/index.js
@@ -0,0 +1,18 @@
+console.log("Hello world");
+
+function sayHello(name) {
+    return `Hello ${name}`;
+}
+
+function addFive(number) {
+    return number + 5;
+}
+
+console.log(
+    sayHello('Arash'),
+);
+
+console.log(
+    '4 + 5 =',
+    addFive(4),
+);
